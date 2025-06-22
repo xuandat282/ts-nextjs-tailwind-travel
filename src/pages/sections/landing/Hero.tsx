4ef6368d794cc7a5d3a65416d65fbe9923330314
khoa47 <bom120805@gmail.com>
2025-06-22 10:55:09 +0700

    Update Hero.tsx

--- src/pages/sections/landing/Hero.tsx
+++ src/pages/sections/landing/Hero.tsx
@@ -21,19 +21,19 @@ const Hero = () => {
         </a>
       </div>
       {/* hero */}
-      <div className="flex flex-col items-center px-2 xs:px-0">
-        <h1 className="text-3xl xs:text-4xl max-w-xs text-slate-100 font-bold text-center md:max-w-lg lg:text-[2.5rem] lg:leading-tight">
-          Journey of discovery and learning
-        </h1>
-        <div className="mt-6">
-          <Link
-            href="/Start"
-            className="border text-slate-100 px-6 py-2 text-lg rounded-full backdrop-blur-sm lg:px-8 lg:py-[12px] lg:hover:px-9 lg:hover:py-[16px] md:hover:px-8 md:hover:py-[12px]  transition-all duration-300"
-          >
-            Get started
-          </Link>
-        </div>
-      </div>
+<div className="flex flex-col items-center px-4 sm:px-6 md:px-8">
+  <h1 className="text-balance text-4xl sm:text-5xl md:text-6xl font-bold text-center text-slate-100 leading-snug max-w-4xl">
+    Journey of discovery and learning
+  </h1>
+  <div className="mt-6">
+    <Link
+      href="/Start"
+      className="border text-slate-100 px-6 py-2 text-lg rounded-full backdrop-blur-sm hover:px-8 hover:py-3 transition-all duration-300"
+    >
+      Get started
+    </Link>
+  </div>
+</div>
       {/* scroll */}
       <div className="hidden xs:flex flex-col items-center px-5 group lg:px-8">
         <div className="w-[22px] h-[22px] rounded-full bg-slate-100 mb-3 group-hover:animate-bounce"></div>
